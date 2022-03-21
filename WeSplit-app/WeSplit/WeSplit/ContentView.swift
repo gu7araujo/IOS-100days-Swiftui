71c02a7813daaf029454542bcdda10332be4b11f
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-03-21 10:23:38 -0300

    Adding first app

---
+++ WeSplit-app/WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+ //
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Gustavo Araujo Santos on 21/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Olá, mundo!")
+            .padding()
+    } 
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit-app/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Gustavo Araujo Santos on 21/03/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
