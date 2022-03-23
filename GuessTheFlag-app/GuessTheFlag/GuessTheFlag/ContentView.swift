1282314a8e52dcb515f6d7c6610272d9aa2d17fa
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-03-23 16:36:38 -0300

    Ending project 2

--- GuessTheFlag-app/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag-app/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,6 +10,9 @@ import SwiftUI
 struct ContentView: View {
     @State private var showingScore = false
     @State private var scoreTitle = ""
+    @State private var scoreValue = 0
+
+    @State private var showingReset = false
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
     @State private var correctAnswer = Int.random(in: 0...2)
@@ -57,7 +60,7 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
 
-                Text("Score: ???")
+                Text("Score: \(scoreValue)")
                     .foregroundColor(.white)
                     .font(.title.bold())
 
@@ -68,23 +71,38 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continue", action: askQuestion)
         } message: {
-            Text("Your score is ???")
+            Text("Your score is \(scoreValue)")
+        }
+        .alert("BEST!!", isPresented: $showingReset) {
+            Button("Reset Game", action: reset)
+        } message: {
+            Text("You are very very good!")
         }
     }
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
             scoreTitle = "Correct"
+            scoreValue += 1
         } else {
-            scoreTitle = "Wrong"
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
         }
 
-        showingScore = true
+        if scoreValue == 8 {
+            showingReset = true
+        } else {
+            showingScore = true
+        }
     }
 
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+    }
+
+    func reset() {
+        askQuestion()
+        scoreValue = 0
     }
 }
 
