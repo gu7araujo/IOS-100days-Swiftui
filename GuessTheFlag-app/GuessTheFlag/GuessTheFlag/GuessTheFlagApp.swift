cfc50affead7599275ca930742c81e59b4ec2efd
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-03-23 14:15:47 -0300

    Guess the flag projet and showing alert messages

---
+++ GuessTheFlag-app/GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Gustavo Araujo Santos on 23/03/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
