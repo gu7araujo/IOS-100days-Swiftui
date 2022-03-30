95c02c058d6845dc861c9058fab097e7abfd31d3
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-03-30 09:56:14 -0300

    Custom modifiers

--- ViewsAndModifiers-app/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers-app/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,25 +7,50 @@
 
 import SwiftUI
 
-struct CapsuleText: View {
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+struct Watermark: ViewModifier {
     var text: String
 
-    var body: some View {
-         Text(text)
-            .font(.largeTitle)
-            .padding()
-            .foregroundColor(.white)
-            .background(.blue)
-            .clipShape(Capsule())
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+             content
+
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
     }
 }
 
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-            CapsuleText(text: "Second")
-        }
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Gustavo Araujo")
     }
 }
 
