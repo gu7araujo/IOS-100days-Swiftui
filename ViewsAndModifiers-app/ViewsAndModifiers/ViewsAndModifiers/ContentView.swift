769a6d29ad52db00b8ef96c19614f51cdc1cf683
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-03-30 09:33:31 -0300

    View composition

--- ViewsAndModifiers-app/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers-app/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,22 +7,25 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var motto1: some View {
-        Text("Draco dormiens")
-    }
-
-    let motto2 = Text("nunquam titillandus")
-
-    @ViewBuilder var spells: some View {
-        VStack {
-            Text("Lumos")
-            Text("Obliviate")
-        }
-    }
+struct CapsuleText: View {
+    var text: String
 
     var body: some View {
-        spells
+         Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundColor(.white)
+            .background(.blue)
+            .clipShape(Capsule())
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+            CapsuleText(text: "Second")
+        }
     }
 }
 
