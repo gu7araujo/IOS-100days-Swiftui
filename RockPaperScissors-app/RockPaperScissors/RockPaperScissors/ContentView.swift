e66909e75ec2e8b9630a8a65c6323b479065b61c
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-01 15:48:47 -0300

    Initial rock paper scissors

---
+++ RockPaperScissors-app/RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -0,0 +1,113 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Gustavo Araujo Santos on 01/04/22.
+//
+
+import SwiftUI
+
+struct Symbol: View {
+    var systemName: String
+    var text: String
+
+    var body: some View {
+        VStack {
+            Image(systemName: systemName)
+            Text(text)
+        }
+    }
+}
+
+struct ContentView: View {
+    var possibleMoves = ["rock", "paper", "scissors"]
+    var systemNameSymbols = ["globe.asia.australia.fill", "newspaper", "scissors"]
+
+    @State private var machineChoice = Int.random(in: 0...2)
+    @State private var shouldWin = Bool.random()
+
+    @State private var scoreValue = 0
+    @State private var attempts = 0
+
+    @State private var showingScore = false
+
+    var body: some View {
+        VStack {
+            Symbol(systemName: systemNameSymbols[machineChoice], text: possibleMoves[machineChoice])
+
+            if shouldWin {
+                Text("Win")
+            } else {
+                Text("Lost")
+            }
+
+            HStack {
+                ForEach(possibleMoves, id: \.self) { move in
+                    Button {
+                        moveTapped(move)
+                    } label: {
+                        Symbol(systemName: systemNameSymbols[possibleMoves.firstIndex(of: move)!], text: move)
+                    }
+                }
+            }
+
+            Text("Score \(scoreValue)")
+        }
+        .alert("End of the Game", isPresented: $showingScore) {
+            Button("Reset Game", action: reset)
+        } message: {
+            Text("Your Score is \(scoreValue)")
+        }
+    }
+
+    func reset() {
+        scoreValue = 0
+        attempts = 0
+        machineChoice = Int.random(in: 0...2)
+        shouldWin.toggle()
+    }
+
+    func moveTapped(_ move: String) {
+        attempts += 1
+        let machineChoiceName = possibleMoves[machineChoice]
+        let userChoice = move
+        var win = false
+
+        if machineChoiceName == "rock" && userChoice == "paper" && shouldWin {
+            win = true
+        } else if machineChoiceName == "rock" && userChoice == "scissors" && !shouldWin {
+            win = true
+        }
+
+        if machineChoiceName == "paper" && userChoice == "scissors" && shouldWin {
+            win = true
+        } else if machineChoiceName == "paper" && userChoice == "rock" && !shouldWin {
+            win = true
+        }
+
+        if machineChoiceName == "scissors" && userChoice == "rock" && shouldWin {
+            win = true
+        } else if machineChoiceName == "scissors" && userChoice == "paper" && !shouldWin {
+            win = true
+        }
+
+        if win {
+            scoreValue += 1
+        } else {
+            scoreValue = scoreValue == 0 ? 0 : scoreValue - 1
+        }
+
+        if attempts == 10 {
+            showingScore = true
+        } else {
+            machineChoice = Int.random(in: 0...2)
+            shouldWin.toggle()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RockPaperScissors-app/RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Gustavo Araujo Santos on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
