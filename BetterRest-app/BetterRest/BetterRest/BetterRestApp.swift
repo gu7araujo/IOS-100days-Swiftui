75a2655d4f7c7f25c901546fb95374971e4510f2
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-04 11:23:58 -0300

    BetterRest app

---
+++ BetterRest-app/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Gustavo Araujo Santos on 04/04/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
