4dc07597b25055bacdd3c722087ce254b2f89721
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-11 09:00:14 -0300

    Creating explict animations

--- Animations-app/Animation/Animation/ContentView.swift
+++ Animations-app/Animation/Animation/ContentView.swift
@@ -8,27 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 1.0
+    @State private var animationAmount = 0.0
 
     var body: some View {
         Button("tap me") {
-//            animationAmount += 1
+            withAnimation {
+                animationAmount += 360
+            }
         }
         .padding(50)
         .background(.red)
         .foregroundColor(.white)
         .clipShape(Circle())
-        .overlay(
-            Circle()
-                .stroke(.red)
-                .scaleEffect(animationAmount)
-                .opacity(2 - animationAmount)
-                .blur(radius: (animationAmount - 1) * 3)
-                .animation(.easeInOut(duration: 1).repeatForever(autoreverses: false), value: animationAmount)
-        )
-        .onAppear {
-            animationAmount = 2
-        }
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 1, z: 0))
 
     }
 }
