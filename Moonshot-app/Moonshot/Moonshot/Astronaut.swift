2d609a14e9f6f81d146aee0bff52d1ed05b7a4f7
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-19 09:16:36 -0300

    Loading a specific kind of Codable data

---
+++ Moonshot-app/Moonshot/Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Gustavo Araujo Santos on 19/04/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
