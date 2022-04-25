30219fa1d0f215f8657018061ed1c3d6d0711b23
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-04-25 13:30:17 -0300

    Drawing initial

---
+++ Drawing-app/Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Gustavo Araujo Santos on 25/04/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
