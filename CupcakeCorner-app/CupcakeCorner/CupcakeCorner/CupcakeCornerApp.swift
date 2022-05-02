652aca0d000e2607f1b7c45b7801e01aade8e265
Gustavo Araujo Santos <garaujo@corp.lifescan.com>
2022-05-02 13:20:48 -0300

    Adding Codable conformance for @Published properties Cupcake project

---
+++ CupcakeCorner-app/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Gustavo Araujo Santos on 02/05/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
